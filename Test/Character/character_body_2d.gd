deda1293d3627765cf3aff11dec418608908c14e
BictorFonseca <143542120+BictorFonseca@users.noreply.github.com>
2023-09-26 23:13:30 -0500

    Added Ladder Object

--- Test/Character/character_body_2d.gd
+++ Test/Character/character_body_2d.gd
@@ -6,13 +6,18 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var on_ladder=false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	print(on_ladder)
+	if on_ladder==true:
+		if Input.is_action_pressed("jump"):
+			velocity.y=-SPEED
+		elif Input.is_action_pressed("down"):
+			velocity.y=SPEED
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
