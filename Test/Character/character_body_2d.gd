5ce663045c6a65cf1d42dc2f4b61a5a11c637bb5
jackmayberry <jack.mayberry@utexas.edu>
2023-09-29 11:40:05 -0500

    Sludge, Created Main Node
    Created the sludge scene that slows the player. Created the main control node for our game.

--- Test/Character/character_body_2d.gd
+++ Test/Character/character_body_2d.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 300.0
+@export var jump_velocity = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,20 +14,20 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 	if on_ladder==true:
 		if Input.is_action_pressed("jump"):
-			velocity.y=-SPEED
+			velocity.y=-speed
 		elif Input.is_action_pressed("down"):
-			velocity.y=SPEED
+			velocity.y=speed
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("run_left", "run_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 
---
+++ Test/Main/Main.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var level_01_scene = preload("res://Levels/Level01/level_01.tscn")
+var player_scene = preload("res://Character/character_body_2d.tscn")
+
+# TEMPORARY CODE
+func _ready():
+	var level = level_01_scene.instantiate()
+	var player = player_scene.instantiate()
+	add_child(level)
+	add_child(player)
+	player.global_position = Vector2(100, 100)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
