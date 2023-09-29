7e761c9b0729a184ca67bddd582aae396a02070f
jackmayberry <jack.mayberry@utexas.edu>
2023-09-29 11:49:37 -0500

    Window size change, cleaned up main script

--- Test/Main/Main.gd
+++ Test/Main/Main.gd
@@ -5,12 +5,15 @@ var player_scene = preload("res://Character/character_body_2d.tscn")
 
 # TEMPORARY CODE
 func _ready():
+	_begin_level_01()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _begin_level_01():
 	var level = level_01_scene.instantiate()
 	var player = player_scene.instantiate()
 	add_child(level)
 	add_child(player)
 	player.global_position = Vector2(100, 100)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
