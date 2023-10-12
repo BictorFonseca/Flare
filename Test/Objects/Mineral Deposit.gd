6dbcb887b033b7c6b29eb95ebfa1225fb642ca3e
BictorFonseca <143542120+BictorFonseca@users.noreply.github.com>
2023-10-12 18:56:34 -0500

    Art&Instruction Details

--- Test/Objects/Mineral Deposit.gd
+++ Test/Objects/Mineral Deposit.gd
@@ -10,3 +10,17 @@ func _process(delta):
 
 func _on_glow_timer_timeout():
 	$PointLight2D2.energy = randf_range(3.0, 3.3)
+	
+
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		$Mineral_Instruction.visible=true
+	pass # Replace with function body.
+
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("Player"):
+		$Mineral_Instruction.visible=false
+	pass # Replace with function body.
--- Test/Objects/lever.gd
+++ Test/Objects/lever.gd
@@ -15,4 +15,11 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		get_node("../../player").lever1=true
+		$Lever_Prompt.visible=true
 	pass # Replace with function body.
+
+func _on_body_exited(body):
+	if body.is_in_group("Player"):
+		$Lever_Prompt.visible=false
+	pass
+	
