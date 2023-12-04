4d92aff84b7e0b388cd44a030b05c291d03c5e7b
SiddhantS2003 <145145209+SiddhantS2003@users.noreply.github.com>
2023-12-03 19:17:30 -0600

    added the animations for l1 falling

--- Test/Levels/Tutorial/cutscene.gd
+++ Test/Levels/Tutorial/cutscene.gd
@@ -4,6 +4,7 @@ var gates_active = true
 var scream = preload("res://Levels/Tutorial/scream.tscn")
 var alpha = 0
 var darken = false
+var falling = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +17,10 @@ func _ready():
 	#$Ladder2/CollisionShape2D.apply_scale(Vector2(1,2))
 	#$Ladder3/CollisionShape2D.apply_scale(Vector2(1,2.2))
 	#$player.flare_lit = false
+	$Trapdoor3.hide()
+	$Trapdoor4.hide()
+	$Trapdoor5.hide()
+	$Trapdoor6.hide()
 	
 	#$Ladder/LadderGuide.apply_scale(Vector2(1,0.6523157208))
 
@@ -26,7 +31,17 @@ func _process(delta):
 		$ColorRect.color = Color("00000000", alpha)
 		if alpha < 0.96:
 			alpha += 0.02
-
+	if falling:
+		$Trapdoor1.hide()
+		$Trapdoor2.hide()
+		$Trapdoor3.show()
+		$Trapdoor4.show()
+		$Trapdoor5.show()
+		$Trapdoor6.show()
+		$Trapdoor3.position.y += 8
+		$Trapdoor4.position.y += 9
+		$Trapdoor5.position.y += 11
+		$Trapdoor6.position.y += 6
 
 
 
@@ -35,6 +50,7 @@ func _on_tutorial_finished_body_entered(body):
 	if body.is_in_group("Player"):
 		var main = get_parent()
 		var scream_instance = scream.instantiate()
+		falling = true
 		main.add_child(scream_instance)
 		await get_tree().create_timer(0.5).timeout
 		darken = true
